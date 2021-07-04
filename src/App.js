1e1d573eca8ad8201704a8ad2b601b2ba5fda60f
eugenehan94 <eugene.han94@gmail.com>
2021-07-04 15:12:04 -0400

    Added functionality for REST API HTTP Methods

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
@@ -44,14 +45,21 @@ function App() {
     setDiaries([...diaries, data]);
   };
 
-  if (loading) {
-    return <p className="loading-text">Data is loading...</p>;
-  }
+  //Edit data
+  const editTask = async (diary) => {
+    let targetId = indexValue + 1;
+    const res = await fetch(`http://localhost:5000/diaries/${targetId}`, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(diary),
+    });
+    const data = res.json();
+    setDiaries([...diaries, data]);
+  };
 
   const onSubmit = (e) => {
     // e.preventDefault();
     if (!title || !date || !entries) {
-      alert("Please complete all input: \n\nTitle \nDate \nEntries");
       e.preventDefault();
       return;
     }
@@ -62,10 +70,22 @@ function App() {
     setEntries("");
   };
 
+  const editSubmit = (e) => {
+    if (!title && !date && !entries) {
+      e.preventDefault();
+      return;
+    }
+    editTask({ title, date, entries });
+  };
+
+  if (loading) {
+    return <p className="loading-text">Data is loading...</p>;
+  }
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path={["/", "/home"]}>
           <Link to="/new_diary">
             <button className="Button">Added new entries</button>
           </Link>
@@ -75,7 +95,17 @@ function App() {
                 <div>
                   <div className="titleAndbutton">
                     <h2>{diary.title}</h2>
-                    <button onClick={() => deleteTask(diary.id)}>Delete</button>
+                    <div>
+                      <button onClick={() => deleteTask(diary.id)}>
+                        Delete
+                      </button>
+                      <button>|</button>
+                      <Link to="edit_diary">
+                        <button onClick={() => setIndexValue(diary.id - 1)}>
+                          Edit
+                        </button>
+                      </Link>
+                    </div>
                   </div>
                   <h3 className="diary-date">{diary.date}</h3>
                   <p className="diary-entry">{diary.entries}</p>
@@ -114,6 +144,47 @@ function App() {
                 <textarea
                   type="text"
                   placeholder="How are you feeling...?"
+                  value={entries}
+                  onChange={(e) => setEntries(e.target.value)}
+                ></textarea>
+              </div>
+              <div>
+                <input type="submit" value="Save" className="save-btn"></input>
+              </div>
+            </form>
+          </div>
+        </Route>
+
+        <Route path="/edit_diary">
+          <Link to="/">
+            <button className="Button">Back to home</button>
+          </Link>
+          <div className="form-container">
+            <h1>Page to Edit Diary</h1>
+            <form onSubmit={editSubmit}>
+              <div>
+                <label>Title:</label>
+                <input
+                  type="text"
+                  placeholder={diaries[indexValue].title}
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label>Date:</label>
+                <input
+                  type="date"
+                  placeholder={diaries[indexValue].date}
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label>Entries:</label>
+                <textarea
+                  type="text"
+                  placeholder={diaries[indexValue].entries}
                   value={entries}
                   onChange={(e) => setEntries(e.target.value)}
                 ></textarea>
